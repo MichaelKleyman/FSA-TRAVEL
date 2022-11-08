64b541a63dfeb48006833ae587d0a42d5c836277
Michael Kleyman <michaelkleyman99@gmail.com>
2022-11-08 15:27:50 -0500

    added reducers

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import airlinesReducer from './airlines'
+import airportsReducer from './airports'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ auth, airlinesReducer, airportsReducer })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
