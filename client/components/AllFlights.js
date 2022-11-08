0f476e13ffa85cca909c7b4251ae58d9ca71327c
WhizBang <Kvance247@gmail.com>
2022-11-08 13:38:16 -0600

    added to Package.json - Create db FSA-TRAVEL

---
+++ client/components/AllFlights.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import Axios from "axios";
+
+const origin = "MSY";
+const destination = "IAD";
+
+function AllFlights() {
+  useEffect(() => {
+    Axios.get(
+      `https://api.travelpayouts.com/v1/prices/calendar?depart_date=2016-11&origin=${origin}&destination=${destination}&calendar_type=departure_date&token=ed36fb1a96dc9c4593b94a42e1a6825a&`
+    )
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return <div className="App">Hello World!</div>;
+}
+
+export default AllFlights;
