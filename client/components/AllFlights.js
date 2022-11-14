930bfae0b6a5928549f934e510ea2da639731537
WhizBang <Kvance247@gmail.com>
2022-11-14 15:49:48 -0600

    minor css changes

--- client/components/AllFlights.js
+++ client/components/AllFlights.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import AddFlight from './AddFlight';
-// import axios from 'axios';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 const luxon = require('luxon');
-// const selected_origin = 'MSY';
-// const selected_destination = 'IAD';
 
 function AllFlights(props) {
   const flights = props.flights;
@@ -16,26 +14,26 @@ function AllFlights(props) {
   };
   console.log(flights);
   return (
-    <div className="card-container">
+    <div className='card-container'>
       {flights.map(([k, flight]) => {
         let dt = luxon.DateTime.fromISO(flight.departure_at);
         let rt = luxon.DateTime.fromISO(flight.return_at);
         return (
           <div key={k}>
-            <div className="card">
-              <div className="card-date">
+            <div className='card'>
+              <div className='card-date'>
                 <p>{dt.toLocaleString(luxon.DateTime.DATETIME_MED)}</p>
               </div>
-              <div className="card-direction-price">
-                <p className="card-destination">
+              <div className='card-direction-price'>
+                <p className='card-destination'>
                   {flight.origin} - {flight.destination}
                 </p>
-                <p className="card-price">${flight.price}</p>
+                <p className='card-price'>${flight.price}</p>
               </div>
-              <div className="card-buttons">
+              <div className='card-buttons'>
                 <AddFlight flight={flight} />
                 <button
-                  className="card-btn2"
+                  className='card-btn2'
                   onClick={() => changeContent(flight)}
                 >
                   Details
@@ -46,41 +44,39 @@ function AllFlights(props) {
         );
       })}
       {popUpToggle && (
-        <div className="pop-up-container" onClick={changeContent}>
-          <div className="pop-up-body" onClick={(e) => e.stopPropagation()}>
-            <div className="pop-up-header">
-              <button className="pop-up-x" onClick={changeContent}>
+        <div className='pop-up-container' onClick={changeContent}>
+          <div className='pop-up-body' onClick={(e) => e.stopPropagation()}>
+            <div className='pop-up-header'>
+              <button className='pop-up-x' onClick={changeContent}>
                 X
               </button>
             </div>
-            <div className="pop-up-content">
+            <div className='pop-up-content'>
               {popUpContent.map((flight) => {
                 let dt = luxon.DateTime.fromISO(flight.departure_at);
                 let rt = luxon.DateTime.fromISO(flight.return_at);
                 return (
-                  <div className="pop-up-card">
-                    <h1 className="pop-up-title">
+                  <div className='pop-up-card'>
+                    <h1 className='pop-up-title'>
                       Flight-{flight.flight_number}: {flight.origin} to{' '}
                       {flight.destination}
                     </h1>
-                    <p>Airline: {flight.airline}</p>
-                    <p>
+                    <p className='pop-up-airline'>Airline: {flight.airline}</p>
+                    <p className='pop-up-origin'>
                       Leaving from: {flight.origin} on{' '}
                       {dt.toLocaleString(luxon.DateTime.DATETIME_MED)}{' '}
                     </p>
-                    <p>
-                      Returning from: {flight.destination} on{' '}
-                      {rt.toLocaleString(luxon.DateTime.DATETIME_MED)}
+                    <p className='pop-up-price'>${flight.price}</p>
+                    <p className='pop-up-description'>
+                      At FSA-TRAVEL we believe that traveling can bring good
+                      into the world, and so our mission is to power global
+                      travel for everyone, everywhere.
                     </p>
-
-                    <p>Round trip price: ${flight.price}</p>
-                    <p>
-                      At FSA-TRAVEL, where saftey is are main concern, believe
-                      that traveling can bring good into the world, and so our
-                      mission is to power global travel for everyone,
-                      everywhere.
-                    </p>
-                    <p>For more information contact at us / / / </p>
+                    <div className='pop-up-icons'>
+                      <FaFacebook size={35} />
+                      <FaTwitter size={35} />
+                      <FaInstagram size={35} />
+                    </div>
                   </div>
                 );
               })}
