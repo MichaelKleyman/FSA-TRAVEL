ecbdd498e27d07f228cd29704a7146a157e47c1b
WhizBang <Kvance247@gmail.com>
2022-11-14 17:00:36 -0600

    added DetailsCard

--- client/components/AllFlights.js
+++ client/components/AllFlights.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import AddFlight from './AddFlight';
 import FlightCard from './FlightCard';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-const luxon = require('luxon');
+import DetailsCard from './DetailsCard';
 
 function AllFlights(props) {
   const flights = props.flights;
@@ -30,32 +28,7 @@ function AllFlights(props) {
             </div>
             <div className='pop-up-content'>
               {popUpContent.map((flight) => {
-                let dt = luxon.DateTime.fromISO(flight.departure_at);
-                let rt = luxon.DateTime.fromISO(flight.return_at);
-                return (
-                  <div className='pop-up-card'>
-                    <h1 className='pop-up-title'>
-                      Flight-{flight.flight_number}: {flight.origin} to{' '}
-                      {flight.destination}
-                    </h1>
-                    <p className='pop-up-airline'>Airline: {flight.airline}</p>
-                    <p className='pop-up-origin'>
-                      Leaving from: {flight.origin} on{' '}
-                      {dt.toLocaleString(luxon.DateTime.DATETIME_MED)}{' '}
-                    </p>
-                    <p className='pop-up-price'>${flight.price}</p>
-                    <p className='pop-up-description'>
-                      At FSA-TRAVEL we believe that traveling can bring good
-                      into the world, and so our mission is to power global
-                      travel for everyone, everywhere.
-                    </p>
-                    <div className='pop-up-icons'>
-                      <FaFacebook size={35} />
-                      <FaTwitter size={35} />
-                      <FaInstagram size={35} />
-                    </div>
-                  </div>
-                );
+                return <DetailsCard flight={flight} />;
               })}
             </div>
           </div>
